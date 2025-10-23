449f468458d98de5ffbf8875699c0a29d50020bc
Marco Schaeck <marco.schaeck@outlook.de>
2025-10-23 14:28:21 +0200

    refactor: deprecations

--- examples/after-transaction/src/main/kotlin/de/emaarco/example/adapter/in/zeebe/SendWelcomeMailWorker.kt
+++ examples/after-transaction/src/main/kotlin/de/emaarco/example/adapter/in/zeebe/SendWelcomeMailWorker.kt
@@ -3,7 +3,7 @@ package de.emaarco.example.adapter.`in`.zeebe
 import de.emaarco.example.application.port.`in`.SendWelcomeMailUseCase
 import de.emaarco.example.domain.SubscriptionId
 import io.camunda.client.annotation.JobWorker
-import io.camunda.zeebe.spring.client.annotation.Variable
+import io.camunda.client.annotation.Variable
 import mu.KotlinLogging
 import org.springframework.stereotype.Component
 import java.util.*
